b7880fdc68dd39d5ea3ec1dff0e51bb1817d5983
Marcin Drzewiecki <marcin.drzewiecki@gmail.com>
2015-05-14 07:47:08 +0200

    changing port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@
     app.use(methodOverride());
 
     // listen (start app with node server.js) ======================================
-    app.listen(8080);
+    app..listen(process.env.PORT || 5000);
     console.log("App listening on port 8080");
 
 
